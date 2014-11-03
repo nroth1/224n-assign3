cc7a1d88bd198486ded800c6e79c41564ff38932
Nat Gunther Roth <nroth@corn22.stanford.edu>
2014-11-02 19:59:11 -0800

    naive baselines

--- java/src/cs224n/corefsystems/AllSingleton.java
+++ java/src/cs224n/corefsystems/AllSingleton.java
@@ -1,25 +1,36 @@
 package cs224n.corefsystems;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import cs224n.coref.ClusteredMention;
 import cs224n.coref.Document;
 import cs224n.coref.Entity;
+import cs224n.coref.Mention;
 import cs224n.util.Pair;
 
 public class AllSingleton implements CoreferenceSystem {
 
 	@Override
 	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
-		// TODO Auto-generated method stub
-
+		return;
 	}
-
+	
+	/*
+	 * Add all entities to their own cluster.
+	 */
 	@Override
 	public List<ClusteredMention> runCoreference(Document doc) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Mention> mentions = doc.getMentions();
+		List<ClusteredMention> clustered = new ArrayList<ClusteredMention>();
+		for(Mention m : mentions){
+			//System.out.println("HEREHEHERHEHERHREH!!!");
+			//Entity single = new Entity(mentions);
+			ClusteredMention single = m.markSingleton();
+			clustered.add(single);
+		}
+		return clustered;
 	}
 
 }
--- java/src/cs224n/corefsystems/OneCluster.java
+++ java/src/cs224n/corefsystems/OneCluster.java
@@ -1,25 +1,32 @@
 package cs224n.corefsystems;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import cs224n.coref.ClusteredMention;
 import cs224n.coref.Document;
 import cs224n.coref.Entity;
+import cs224n.coref.Mention;
 import cs224n.util.Pair;
 
 public class OneCluster implements CoreferenceSystem {
 
 	@Override
 	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
-		// TODO Auto-generated method stub
-
+		return;
 	}
 
 	@Override
 	public List<ClusteredMention> runCoreference(Document doc) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Mention> mentions = doc.getMentions();
+		List<ClusteredMention> clustered = new ArrayList<ClusteredMention>();
+		Entity single = new Entity(mentions);
+		for(Mention m : mentions){
+			ClusteredMention singleClust = m.markCoreferent(single);
+			clustered.add(singleClust);
+		}
+		return clustered;
 	}
 
 }
